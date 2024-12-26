ca8f83858c68ffa4bf37cf87aafed7f02012f86f
Louis Sullivan <Louis.Sullivan1@ibm.com>
2024-12-26 02:42:01 +0000

    Allow all cors

--- src/index.js
+++ src/index.js
@@ -13,11 +13,7 @@ const app = express();
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
-app.use(cors({
-    origin: 'http://localhost:3001',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
